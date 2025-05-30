4c1638970028df1ebfd112e508cccad5d3e9d261
Your Name <arturkiekow@gmail.com>
2025-05-29 22:12:17 -0300

    adicionando a opção de adicionar cliente na tabela, e começando a implementar a funcionalidade de excluir

--- script.js
+++ script.js
@@ -1,37 +1,57 @@
+const tabelaClientes = document.querySelector("#tabelaCLientes");
+let id = 1;
 
-let arquivo = fetch("locadora.json").then((res) => {
+fetch("locadora.json").then((res) => {
     return res.json();
 }).then((info)=>{
     populaTabelaClientes(info.clientes);
     populaTabelaCarros(info.carros);
-    populaTabelaLocacoes(info.locacoes)
+    populaTabelaLocacoes(info.locacoes);
+
+    const botoesExcluir = document.querySelectorAll(".botaoExcluir");
+
+    botoesExcluir.forEach(botao => {
+        botao.addEventListener("click", () => {
+            const idCliente = botao.closest("tr").dataset.id;
+            console.log(idCliente);
+            
+            
+        })
+        
+    })
+
 });
+
+function adicionaClienteNaTabela(cliente){
+    const tr = document.createElement('tr');
+    tr.dataset.id = cliente.id;
+
+    for (const atributo in cliente){
+        const td = document.createElement("td");
+        td.setAttribute("class", atributo);
+        td.appendChild(document.createTextNode(cliente[atributo]))
+        tr.appendChild(td);      
+    }
+    
+    const botaoAtualizar = document.createElement("button");
+    botaoAtualizar.appendChild(document.createTextNode("Atualizar"));
+    botaoAtualizar.classList.add("botaoAtualizar");    
+    tr.appendChild(botaoAtualizar);
+
+    const botaoExcluir = document.createElement("button");
+    botaoExcluir.appendChild(document.createTextNode("Excluir"));
+    botaoExcluir.classList.add("botaoExcluir");
+    tr.appendChild(botaoExcluir);
+
+    tabelaClientes.appendChild(tr);
+    id++;
+}
 
 
 function populaTabelaClientes(clientes){
-    const tabelaClientes = document.querySelector("#tabelaCLientes");
 
-     clientes.forEach(cliente => {
-        const tr = document.createElement('tr');
-        tr.setAttribute("id", cliente.id)
-        
-        for (const atributo in cliente){
-            const td = document.createElement("td");
-            td.setAttribute("class", atributo);
-            td.appendChild(document.createTextNode(cliente[atributo]))
-            tr.appendChild(td);      
-        }
-
-        const botaoAtualizar = document.createElement("button");
-        botaoAtualizar.appendChild(document.createTextNode("Atualizar"));
-        botaoAtualizar.setAttribute("class", "botaoAtualizar");
-        tr.appendChild(botaoAtualizar);
-
-        const botaoExcluir = document.createElement("button");
-        botaoExcluir.appendChild(document.createTextNode("Excluir"));
-        botaoExcluir.setAttribute("class", "botaoExcluir");
-        tr.appendChild(botaoExcluir);
-        tabelaClientes.appendChild(tr);
+    clientes.forEach(cliente => {
+        adicionaClienteNaTabela(cliente);
     }); 
 
 }
@@ -54,6 +74,39 @@ botoesAdicionar.forEach(botao => {
     })
 });
 
+var botoesFechar = document.querySelectorAll(".botaoFechar");
+
+botoesFechar.forEach(botao => {
+    botao.addEventListener("click", () => {
+        const modalId = botao.getAttribute("modal");
+        const modal = document.getElementById(modalId);
+        modal.close();
+    })
+});
 
 
-document.querySelector("#adicionar-clientes").showModal();
+const botaoAdicionarCliente = document.querySelector("#botaoAdicionarCliente");
+
+botaoAdicionarCliente.addEventListener("click", adicionarCliente);
+
+
+function adicionarCliente(){
+    const nome = document.querySelector("#nome").value;
+    const cpf = document.querySelector("#cpf").value;
+    const telefone = document.querySelector("#telefone").value;
+    const email = document.querySelector("#email").value;
+
+    console.log(nome, cpf, telefone, email);
+    
+    const cliente = {
+        id: id,
+        nome: nome,
+        cpf: cpf,
+        telefone: telefone,
+        email: email,
+    }
+
+    adicionaClienteNaTabela(cliente);
+}
+
+
