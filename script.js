10e5acb44888511b8e36b08f9b2243317766ccb0
Your Name <arturkiekow@gmail.com>
2025-05-31 16:36:38 -0300

    armazenando clientes no local storage

--- script.js
+++ script.js
@@ -1,28 +1,44 @@
-const tabelaClientes = document.querySelector("#tabelaCLientes");
-let id = 1;
+let id = 11;
 
 fetch("locadora.json").then((res) => {
     return res.json();
 }).then((info)=>{
+
+    //populaTabelaCarros(info.carros);
+    //populaTabelaLocacoes(info.locacoes);
+
+    salvaClientesLocalStorage(info.clientes);
     populaTabelaClientes(info.clientes);
-    populaTabelaCarros(info.carros);
-    populaTabelaLocacoes(info.locacoes);
-
-    const botoesExcluir = document.querySelectorAll(".botaoExcluir");
-
-    botoesExcluir.forEach(botao => {
-        botao.addEventListener("click", () => {
-            const idCliente = botao.closest("tr").dataset.id;
-            console.log(idCliente);
-            
-            
-        })
-        
-    })
-
 });
 
+function salvaClientesLocalStorage(clientes){
+    window.localStorage.removeItem("clientes")
+    window.localStorage.setItem("clientes", JSON.stringify(clientes));
+}
+
+function recuperaClientes(){
+    return JSON.parse(window.localStorage.getItem("clientes"));
+}
+
+function populaTabelaClientes(){
+    limpaTabelaClientes();
+    let clientes = JSON.parse(window.localStorage.getItem("clientes"));
+    
+    clientes.forEach(cliente => {
+        adicionaClienteNaTabela(cliente);
+    }); 
+}
+
+function limpaTabelaClientes(){
+    const tabelaClientes = document.querySelector("#tabelaCLientes");
+
+    while (tabelaClientes.firstChild) {
+        tabelaClientes.removeChild(tabelaClientes.firstChild);    
+    }
+}
+
 function adicionaClienteNaTabela(cliente){
+    const tabelaClientes = document.querySelector("#tabelaCLientes");
     const tr = document.createElement('tr');
     tr.dataset.id = cliente.id;
 
@@ -35,26 +51,19 @@ function adicionaClienteNaTabela(cliente){
     
     const botaoAtualizar = document.createElement("button");
     botaoAtualizar.appendChild(document.createTextNode("Atualizar"));
-    botaoAtualizar.classList.add("botaoAtualizar");    
+    botaoAtualizar.classList.add("botaoAtualizar");
+    //botaoAtualizar.addEventListener("click", atualizarcliente);    
     tr.appendChild(botaoAtualizar);
 
     const botaoExcluir = document.createElement("button");
     botaoExcluir.appendChild(document.createTextNode("Excluir"));
     botaoExcluir.classList.add("botaoExcluir");
+    //botaoExcluir.addEventListener("click", excluirCliente)
     tr.appendChild(botaoExcluir);
 
     tabelaClientes.appendChild(tr);
-    id++;
 }
 
-
-function populaTabelaClientes(clientes){
-
-    clientes.forEach(cliente => {
-        adicionaClienteNaTabela(cliente);
-    }); 
-
-}
 function populaTabelaCarros(carros){
     //console.log("CARROS");
     //console.log(carros);
@@ -89,7 +98,6 @@ const botaoAdicionarCliente = document.querySelector("#botaoAdicionarCliente");
 
 botaoAdicionarCliente.addEventListener("click", adicionarCliente);
 
-
 function adicionarCliente(){
     const nome = document.querySelector("#nome").value;
     const cpf = document.querySelector("#cpf").value;
@@ -97,7 +105,9 @@ function adicionarCliente(){
     const email = document.querySelector("#email").value;
 
     console.log(nome, cpf, telefone, email);
-    
+
+    let clientes = recuperaClientes();
+
     const cliente = {
         id: id,
         nome: nome,
@@ -106,7 +116,13 @@ function adicionarCliente(){
         email: email,
     }
 
-    adicionaClienteNaTabela(cliente);
+    clientes.push(cliente);
+
+    salvaClientesLocalStorage(clientes)
+    console.log(clientes);
+
+    populaTabelaClientes();
+    id++;
 }
 
 
